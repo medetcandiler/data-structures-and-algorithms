b7fe8c39b50c36de0320b5315ef03b0d257fa746
Medetcan Diler <medetdiler1996@gmail.com>
2024-01-08 14:55:29 +0300

    Completed List constructor

--- chapter-2-lists/script.js
+++ chapter-2-lists/script.js
@@ -2,13 +2,49 @@ function List() {
   this.listSize = 0;
   this.pos = 0
   this.data = [];
-  // this.clear = clear;
   this.find = find;
   this.append = append;
   this.remove = remove;
   this.length = length;
   this.toString = toString;
   this.insert = insert;
+  this.removeAll = removeAll;
+  this.isContained = isContained;
+  this.front = front;
+  this.end = end;
+  this.prev = prev;
+  this.next = next;
+  this.currPos = currPos;
+  this.moveTo = moveTo;
+  this.getElement = getElement;
+}
+
+function front() {
+  this.pos = 0
+}
+function end() {
+  this.pos = this.data.length;
+}
+function prev() {
+  if (this.pos > 0) {
+    --this.pos
+  }
+}
+function next() {
+  if (this.pos < this.data.length) {
+    ++this.pos
+  }
+}
+function currPos() {
+  return this.pos
+}
+function moveTo(position) {
+  if(position > 0 && position < this.data.length ) {
+    this.pos = position
+  }
+}
+function getElement() {
+  return this.data[this.pos];
 }
 
 function append(el) {
@@ -29,21 +65,6 @@ function insert(el, after) {
   }
   return false
 }
-
-
-
-
-
-// function findAt(el) {
-//   for (let i = 0; i < this.data.length; i++) {
-//     if (this.data[i] === el) {
-//       console.log(i)
-//       return i;
-//     }
-//   }
-//   return -1
-// }
-
 function find(el) {
   let index = this.data.indexOf(el);
   return index
@@ -62,9 +83,16 @@ function remove(el) {
   // this.data = newList
   // this.listSize -= 1;
 }
-
-
-
+function removeAll() {
+  delete this.data;
+  this.data = [];
+  this.listSize = this.pos = this.data.length;
+}
+function isContained(el) {
+  const isFounded = this.data.find(data => data === el);
+  if (!isFounded) return false;
+  return true
+}
 
 const tryList = new List();
 tryList.append('medet')
@@ -72,5 +100,7 @@ tryList.append('aybike')
 tryList.append('mia')
 tryList.remove('aybike')
 tryList.insert('aybike', 'mia')
+console.log(tryList.getElement())
+
 
 console.log(tryList);
